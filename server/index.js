64ae5cc669ed495c7729be5e0d23d46c18324cbd
MajorTwip <majortwip@comstock.ch>
2020-09-01 21:52:38 +0200

    Implemented combined http/ws-server

--- server/index.js
+++ server/index.js
@@ -1,8 +1,28 @@
 /*
 Cannonistic by O.S. and Y.vK, done at the Bsc Inf @ FFHS (WenE)
-/*
+*/
+
+const PORT = 80
 
 
-//Starting the static files server. Serving from /static/
-import 'serve_static.js';
+//Define servers
+var express = require('express');
+var app = express();
+var expressws = require('express-ws')(app);
+
+
+//Defining the static files server. Serving from /static/
+app.use(express.static('static'));
+
+//Defining the WS server.
+app.ws('/', function(ws, req) {
+    ws.on('message', function(msg) {
+      console.log(msg);
+    });
+    console.log('socket', req.testing);
+  });
+
+app.listen(PORT, () => {
+    console.log(`Server listening at port ${PORT}`)
+  })
 
