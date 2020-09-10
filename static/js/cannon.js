767beb1074d53e67e0242ac6eb160b90ef0b1b77
olaf <olaf.schmidt@students.ffhs.ch>
2020-09-10 19:54:49 +0200

    bullet trajectory adjusted

--- static/js/cannon.js
+++ static/js/cannon.js
@@ -8,13 +8,12 @@ var width = canvas.width = window.innerWidth - 100;
 var height = canvas.height = window.innerHeight - 100;
 
 // Trajectory variables
-var pos = { x: 100, y: height - 100 };
-var velo = 10,
-    corner = 50,
-    rad = 2;
+var pos = { x: -95, y: -500 };
+var velo = 0;
+var rad = 2;
 var ball = { x: pos.x, y: pos.y };
-var time = 0.1;
-var angle = math.PI/4;
+
+var angle = 0.91;
 var gravity = 9.81;
 var vuln = 20;
 
@@ -26,7 +25,7 @@ var scalingFactor = 0.998;
 
 // Ready to go
 $(document).ready(function(){
-    drawCannons ();
+    drawCannons();
     handleInput();
 });
 
@@ -81,7 +80,7 @@ function drawCannons(){
     cannon_with = cannon_resized.width;
 
     ctx.clearRect(0,0,canvas.width,canvas.height);
-    ctx.save();
+    //ctx.save();
     //ctx.drawImage(cannon_mirrored, x_pos, y_pos, cannon_height, cannon_with);
     //ctx.drawImage(barrel_mirrored,x_pos, y_pos, cannon_height, cannon_with);
 
@@ -92,9 +91,9 @@ function drawCannons(){
     // draw left cannon
     ctx.drawImage(cannon_resized, x_pos, y_pos, cannon_height, cannon_with);
     ctx.drawImage(barrel_resized, x_pos, y_pos, cannon_height, cannon_with);
-    ctx.restore();
+    //ctx.restore();
 }
-
+/*
 function elevateBarrel(image, x, y, scale, degree){
     ctx.clearRect(0,0,canvas.width,canvas.height);
     ctx.save();
@@ -108,7 +107,7 @@ function elevateBarrel(image, x, y, scale, degree){
     ctx.restore();
     //drawCannons();
 }
-
+*/
 
 // Elevation of Tube
 function elevation(angle){
@@ -117,41 +116,41 @@ function elevation(angle){
 }
 
 // Power of the shot
-var allowVelo = false;
-function startVelo(){
-    if (allowVelo && velo < 100){
+var loadVelo = false;
+var lv;
+function loadVel(){
+    if (loadVelo && velo < 100){
         velo ++;
-        setTimeout("startVelo()", 20);
+        lv = setTimeout("loadVel()", 20);
     }
-    console.log(velo);
+    console.log("velo" + velo);
 }
 
 function stopVelo(){
-    allowVelo = false;
+    loadVelo = false;
 }
 
 
 var moveX = -1;
-var moveY = -1;//-(gravity/(2*velo**2*math.cos(angle)**2))*ball.x**2 + math.tan(angle)*ball.x;
+var x = 0;
+var ballYStart = height - 200;
 // Calculates the trajectory of the bullet
 function trajectory(){
-    time = time + 0.1;
-
-    //var moveX = (-velo * Math.cos(angle) * time)/width;
-    //var moveY = (velo * Math.sin(angle) * time - gravity/2*time)/height;
-
+    x += 1;
+    var moveY = -1*(x*math.tan(angle) - (gravity*x**2)/(2*(velo**2)*math.pow(math.cos(angle),2)));
+    console.log("First: " + moveY);
 
 
-    console.log(moveX, moveY);
-    if (ball.x > canvas.width - rad || ball.x < rad ||
-        ball.y > canvas.height - rad || ball.y < rad){
-            stopFire();
-    }
-
-    //ball.x += moveX;
     ball.x += moveX;
-    ball.y += moveY;
-    //console.log("X:" + ball.x + " Y:" + ball.y);
+    ball.y = moveY + ballYStart;
+
+    console.log(canvas.heigt);
+    //if (ball.y < canvas.height) {
+    //    stopFire();
+    //}
+
+    console.log("X: " + ball.x + " Y: " + ball.y);
+
 }
 
 // Drawing the Bullet
@@ -167,8 +166,8 @@ function drawBullet(){
 handleInput = function() {
     $("#can_game").bind("mousedown", function(e){
         console.log("down");
-        allowVelo = true;
-        startVelo();
+        loadVelo = true;
+        loadVel();
      });
 
     $("#can_game").bind("mousemove", function(e){
@@ -195,7 +194,7 @@ handleInput = function() {
 
         //drawImage(cannon_left, cannon_left.width/2, cannon_left.height/2,0.05,degree);
         //if (degree > 100 && degree < 110) {
-        elevateBarrel(barrel_mirrored, - cannon_with + 335 , height - 110, 0.1, degree) ;
+        //elevateBarrel(barrel_mirrored, - cannon_with + 335 , height - 110, 0.1, degree) ;
         //}
         //drawRotated(cannon_left, degree);
         //cannon.style.transform = "rotate("+ degree +"deg)";
@@ -205,9 +204,10 @@ handleInput = function() {
 
     $("#can_game").bind("mouseup", function(e){
         console.log("up");
+        clearInterval(lv);
         stopVelo();
         startShoot = setInterval(shoot, 10);
-        velo = 0;
+        //velo = 0;
         });
 };
 
