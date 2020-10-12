ba245bdf8e1009e2656b63e2e5e9ac5cace6f09a
MajorTwip <majortwip@comstock.ch>
2020-10-12 21:28:48 +0200

    implemented collision detection

--- server/gamelogic.js
+++ server/gamelogic.js
@@ -1,3 +1,5 @@
+var v0scale = 1/100;
+
 class Game {
     constructor(gameid1, gameid2) {
         this.id = undefined;
@@ -17,11 +19,35 @@ class Game {
         }
     }
 
+    calculateTrajectory(gunX, gunY, elevation, v0, wind, checkCollision){
+        var traj = new Object();
+        traj.trajectory = new Array();
+        var eleInRad = elevation / 1600 * Math.PI / 2; //Oben = 0, Rechts = 1/2 Pi
+        var x = gunX + (37 * Math.sin(eleInRad)); //add barrel
+        var y = gunY + (37 * Math.cos(eleInRad));
+        var vx = Math.sin(eleInRad) * v0 * v0scale;
+        var vy = Math.cos(eleInRad) * v0 * v0scale;
+
+        var timeline = 0
+
+        traj.trajectory.push({"t":timeline,"x":x,"y":y}); // add first point just after the barrel
+
+        while(!checkCollision(x,y)){
+            timeline += 1;
+            x=x+(vx/10); //move by tenth of a second (artillery standard)
+            y=y+(vy/10);
+            vy = vy - (9.81 / 10) // add gravity
+            vx = vx + (wind / 1000) // add wind (factor to play)  
+            traj.trajectory.push({"t":timeline,"x":x,"y":y}); // add first point just after the barrel
+        }
+        return traj;
+    }
+
     getTrajectory(gunX, gunY, elevation, v0, wind){
         switch (this.level) {
             case 1:
                 var level1 = require("./levels/level1");
-                return level1.getTrajectory(gunX, gunY, elevation, v0, wind);
+                return this.calculateTrajectory(gunX, gunY, elevation, v0, wind, level1.checkCollision);
                 break;
         }
         return undefined;
--- server/levels/level1.js
+++ server/levels/level1.js
@@ -5,27 +5,26 @@ function getInitialGunStates(gameid1,gameid2){
     guns[0].owner = gameid1;
     guns[0].health = 1024;
     guns[0].x=50;
-    guns[0].y=0;
+    guns[0].y=100;
     guns[1] = new Object();
     guns[1].owner = gameid2;
     guns[1].health = 1024;
     guns[1].gunnr = 1;
-    guns[1].x=0;
-    guns[1].y=4096-50;
+    guns[1].x=4096-50;
+    guns[1].y=100;
     return guns;
 }
 
-function getTrajectory(gunX, gunY, elevation, v0, wind, g=9.81){
-    var traj = new Object();
-    traj.trajectory = new Array();
-    traj.trajectory.push({"t":0,"x":gunX,"y":gunY});
-    traj.impactX = -1;
-    traj.impactY = -1;
-    return traj;
+function checkCollision(x,y){
+    if(y<=100) return true; //Below Horizon
+    if(Math.abs(x-50)<30 || Math.abs(x-(4096-50))<30){ //Koord X is less than 30Px from a guncenter
+        if(Math.abs(y-100)<20) return true; // Koord Y is less than 20Px from guncenter. This gives an 60x40px HitBox
+    }
+    return false;
 }
 
 
 module.exports = {
     getInitialGunStates:getInitialGunStates,
-    getTrajectory:getTrajectory
+    checkCollision:checkCollision
 }
