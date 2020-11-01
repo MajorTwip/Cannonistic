0401f9866641bf26e61424305024caec64639872
MajorTwip <majortwip@twipnet.ch>
2020-11-01 12:44:53 +0100

    Add type to return of newturn

--- server/ws_turnhandler.js
+++ server/ws_turnhandler.js
@@ -6,6 +6,7 @@ async function newTurn(msg, sock){
     console.log("new turn...")
     var game = await db.getGame(sock.gameid);
     game.doTurn(msg,sock);
+    game.type = "turn";
     db.saveGame(game);
     directory.sendToGame(game.id, JSON.stringify(game));
 }
